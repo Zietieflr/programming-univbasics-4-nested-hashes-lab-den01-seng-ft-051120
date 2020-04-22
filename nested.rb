aca44a15535aa06b6dc0d92c60aa1bc201b1d2ef
Logan McGuire <lrmcguire93@gmail.com>
2020-04-22 17:35:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,8 @@ def dennis_ritchies_language
     }
 programmer_hash[:dennis_ritchie][:languages][0];
 #research ways to make "0" dynamic
-programmer_hash[:dennis_ritchie][:languages][index] 
+dynamic = ""
+programmer_hash[:dennis_ritchie][:languages][].length > |index| ? dynamic = dynamic + programmer_hash
 end
 
 def adding_matz
