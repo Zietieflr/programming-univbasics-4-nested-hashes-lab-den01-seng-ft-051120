5d9c441e9d112b4893219a2a6dc5dbd639a70420
Logan McGuire <lrmcguire93@gmail.com>
2020-04-22 17:45:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
     }
 #programmer_hash[:dennis_ritchie][:languages][0];
 #research ways to make "0" dynamic
-dynamic = ""
+dynamic = "";
 |index| programmer_hash[:dennis_ritchie][:languages][].length > index ? 
   (dynamic = dynamic + programmer_hash[:dennis_ritchie][:languages][index]) : ;
   dynamic;
